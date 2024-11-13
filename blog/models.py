ef09a43d6c2281901e3d9faa5165ec3cd2a0d93d
Matthew Cowen <svlnsr3f@students.codeinstitute.net>
2024-11-13 15:13:01 +0000

    Created Comment model

--- blog/models.py
+++ blog/models.py
@@ -19,3 +19,15 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     exerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
+
+class Comment(models.Model):
+
+    post = models.ForeignKey(
+    Post, on_delete=models.CASCADE, related_name="post_comments"
+    )
+    author = models.ForeignKey(
+    User, on_delete=models.CASCADE, related_name="user_comments"
+    )
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField()
