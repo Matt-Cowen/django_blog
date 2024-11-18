9590bc8db2e0a00485d10aa196747ce73f41bd98
Matthew Cowen <svlnsr3f@students.codeinstitute.net>
2024-11-18 10:06:35 +0000

    blog views indentation fixed

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import generic
 from .models import Post
@@ -10,7 +10,7 @@ class PostList(generic.ListView):
     template_name = "blog/index.html"
     paginate_by = 6
 
-    def post_detail(request, slug):
+def post_detail(request, slug):
     # """
     # Display an individual :model:`blog.Post`.
 
